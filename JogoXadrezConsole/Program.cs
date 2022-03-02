1d0563c3f66d9182e777ec5e5e56cc7591dddeea
Leonardo Bortolini Trombini <leeonardobt@gmail.com>
2022-03-02 19:48:05 -0300

    Iniciada a criação do tabuleiro e criação de peças.

--- JogoXadrezConsole/Program.cs
+++ JogoXadrezConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using tabuleiro;
+using xadrez;
 
 namespace JogoXadrezConsole
 {
@@ -6,7 +8,13 @@ namespace JogoXadrezConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 5));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(3, 4));
+
+            Tela.imprimirTabuleiro(tab);
         }
     }
 }
